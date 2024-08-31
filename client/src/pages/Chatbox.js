14f51ff02c83d7a11751a275786eb2de90355f28
Devansh Gulati <88591733+dddragons0803@users.noreply.github.com>
2024-08-31 21:44:11 +0530

    Update Chatbox.js

--- client/src/pages/Chatbox.js
+++ client/src/pages/Chatbox.js
@@ -5,7 +5,8 @@ import io from "socket.io-client";
 import Sidebar from '../components/Sidebar';
 import Main from '../components/Main';
 // const socket = io.connect('https://wlone.onrender.com');
-const socket = io.connect('http://localhost:5000/');
+// const socket = io.connect('http://localhost:5000/');
+ const socket = io.connect('https://chat-box-2-pcpn.onrender.com/');
 
 const Chatbox = () => {
     return (
